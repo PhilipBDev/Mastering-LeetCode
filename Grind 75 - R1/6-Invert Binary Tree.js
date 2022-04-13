afeb2badaf1603bb020a885c32bd593e3c9cebdc
Philip <philipbdev@gmail.com>
2022-04-13 13:58:34 -0400

    Question 9

--- Grind 75 - R1/6-Invert Binary Tree.js
+++ Grind 75 - R1/6-Invert Binary Tree.js
@@ -7,10 +7,10 @@ Resources:
 https://leetcode.com/problems/invert-binary-tree/discuss/399221/Clean-JavaScript-iterative-DFS-BFS-solutions
 https://leetcode.com/problems/invert-binary-tree/discuss/1725229/JS-Heavily-Commented-Solution-Using-BFS
 
-1. Based off [2, 1, 3] -- this.val would represent 2, this.left would represent 1, this.right would represent 3.
-2. Recursion: Need more practive, but the gist is that we traverse down each side of the tree, then switch the values of left and right.
-3. DFS (Stack): First we insert the inputted root into a stack. While there is something within the stack.
-4. BFS: See comments from resource within code below.
+  1. Based off [2, 1, 3] -- this.val would represent 2, this.left would represent 1, this.right would represent 3.
+  2. Recursion: Need more practive, but the gist is that we traverse down each side of the tree, then switch the values of left and right.
+  3. DFS (Stack): First we insert the inputted root into a stack. While there is something within the stack.
+  4. BFS: See comments from resource within code below.
 
 */
 
